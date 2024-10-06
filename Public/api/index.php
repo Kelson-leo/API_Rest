630ecef8e7a627786ab65ef59a6cb7cfebed1dd9
user <kelsonlean@gmail.com>
2024-10-06 14:13:45 -0300

    first route

--- Public/api/index.php
+++ Public/api/index.php
@@ -15,8 +15,28 @@ $id = null; //param
 $data = null;
 $method = $_SERVER["REQUEST_METHOD"]; //POST, PUT, DELETE and GET
 $uri = $_SERVER["REQUEST_URI"];
+$unsetCount = 4;
+
+//TRATA A URI
+$ex = explode("/", $uri);
+
+for ($i=0; $i < $unsetCount; $i++) { 
+	unset($ex[$i]);
+}
+
+$ex = array_filter(array_values($ex)); //filtra se tem vazio, e começa do zero os indice do array
+
+if(isset($ex[0])) {
+	$controller = $ex[0];
+}
+
+if(isset($ex[1])) {
+	$id = $ex[1];
+}
+
+var_dump($ex);
 
 parse_str(file_get_contents('php://input'), $data); //parse_str é pra transofmrar em array
-echo json_encode(["tipo"=>$method, "data"=>$data, "uri"=>$uri]);
+echo json_encode(["controller"=>$controller, "id"=>$id]);
 
  ?>
