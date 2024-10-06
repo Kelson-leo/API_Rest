b685352c1854442f69ff962c7a3df40dc3d2281c
user <kelsonlean@gmail.com>
2024-10-06 13:10:12 -0300

    REQUEST_METHOD test

--- Public/api/index.php
+++ Public/api/index.php
@@ -1,8 +1,20 @@
 <?php 
 
-require_once("../../vendor/autoload.php");
-use App\Controller\GameController;
+require_once("../../vendor/autoload.php"); //agora tem acesso a "App"
 
-$gameController = new GameController();
+//HEADER 
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$controller = null;
+$id = null; //param
+$data = null;
+$method = $_SERVER["REQUEST_METHOD"]; //POST, PUT, DELETE and GET
+
+echo json_encode(["tipo"=>$method]);
 
  ?>
